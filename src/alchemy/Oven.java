cf3e03f93708d3741626b9d30989f63637771559
csvBoglarka <csvboglarka@gmail.com>
2026-05-14 15:57:30 +0200

    oven klaar behalve de containers

--- src/alchemy/Oven.java
+++ src/alchemy/Oven.java
@@ -24,7 +24,7 @@ public class Oven extends Device {
      * Container
      **********************************************************/
 
-    private IngredientContainer container; //
+    private IngredientContainer container;
 
     public IngredientContainer getContainer() {
         return container;
@@ -40,13 +40,49 @@ public class Oven extends Device {
         return temperature;
     }
 
-    private boolean canItBeHeated(){
+    private int errorOnTemperature(){
+        int[] marginOfError = {-5, -4, -3, -2, -1, 0, 1, 2, 3, 4, 5};
+        int randomIndex = (int)(Math.random() * marginOfError.length);
+        return marginOfError[randomIndex];
+    }
+
+    private void deviateTemperature(){
+        int error = errorOnTemperature();
         int coldness = this.getTemperature()[0];
         int hotness = this.getTemperature()[1];
+        if (coldness != 0){
+            coldness = coldness - error;
+        } else{
+            hotness = hotness - error;
+        }
+        if (coldness < 0) {
+            hotness = Math.abs(coldness);
+            coldness = 0;
+        }
+        if (hotness < 0) {
+            coldness = Math.abs(hotness);
+            hotness = 0;
+        }
+        this.temperature = new int[]{coldness, hotness};
+    }
+
+    private boolean canItBeHeated(){
+        this.deviateTemperature();
+        int coldnessOven = this.getTemperature()[0];
+        int hotnessOven = this.getTemperature()[1];
         AlchemicIngredient ingredient = this.getContainer().getIngredient();
-        int coldness = ingredient.getColdness();
-        int hotness = ingredient.getHotness();
-        // als temp van ingredien lager dan van temp van over --> true
+        int coldnessIng = ingredient.getColdness();
+        int hotnessIng = ingredient.getHotness();
+        if (coldnessOven == 0){
+            if (hotnessIng <= hotnessOven){
+                return true;
+            }
+            return false;
+        }
+        if (coldnessIng >= coldnessOven){
+            return true;
+        }
+        return false;
     }
 
     /**********************************************************
@@ -54,12 +90,13 @@ public class Oven extends Device {
      **********************************************************/
 
     protected void heat(){
-        AlchemicIngredient ingredient = this.getAlchemicIngredient();
-        if canItBeHeated(){
+        AlchemicIngredient ingredient = this.getContainer().getIngredient();
+        //oude container moet vernietigd worden
+        if (this.canItBeHeated()){
             ingredient.addPrefixHeated();
-            ingredient.changeTemperature();
-
+            ingredient.changeTemperature(this.getTemperature());
         }
+        //nieuwe container moet gemaakt worden
     }
 
 }
