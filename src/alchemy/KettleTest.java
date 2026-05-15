e79eb98ca403a01e5373c6ac13accec6d7b64e0e
Adelina <vozianuadelina@gmail.com>
2026-05-15 17:12:37 +0200

    kettle test

---
+++ src/alchemy/KettleTest.java
@@ -0,0 +1,96 @@
+package alchemy;
+import org.junit.*;
+
+import java.awt.*;
+import java.util.ArrayList;
+
+/**
+ * A JUnit (4) test class for testing the non-private methods of the Kettle Class.
+ *
+ * @author  Adelina Vozianu
+ * @author  Boglárka Csorba-Vitus
+ * @version 1.0
+ */
+public class KettleTest {
+    IngredientType flower, wallMold, floorMold;
+    AlchemicIngredient oneIng, moreOfTheSame, wall, floor;
+    IngredientContainer containerOneIng, containerMoreOfTheSame, containerWall, containerFloor;
+    Laboratory lab;
+    Kettle kettleOneIng, kettleFlower, kettleMix;
+
+
+    @Before
+    public void setUp() {
+        this.flower = new IngredientType("Flower", State.LIQUID, new int[]{0, 11});
+        this.wallMold = new IngredientType("Wall Mold", State.LIQUID, new int[]{0, 100});
+        this.floorMold =  new IngredientType("Floor Mold", State.POWDER, new int[]{100, 0});
+
+        this.oneIng = new AlchemicIngredient(flower, 10, UnitOfQuantity.SPOON);
+        this.containerOneIng = new IngredientContainer(UnitOfQuantity.BARREL, oneIng);
+        ArrayList<IngredientContainer> container = new ArrayList<>();
+        container.add(containerOneIng);
+        this.kettleOneIng = new Kettle(container);
+
+        this.moreOfTheSame = new AlchemicIngredient(flower, 2, UnitOfQuantity.BOTTLE);
+        this.containerMoreOfTheSame = new IngredientContainer(UnitOfQuantity.BARREL, moreOfTheSame);
+        ArrayList<IngredientContainer> container2 = new ArrayList<>();
+        container2.add(containerOneIng);
+        container2.add(containerMoreOfTheSame);
+        this.kettleFlower = new Kettle(container2);
+
+        this.wall = new AlchemicIngredient(wallMold, 25, UnitOfQuantity.SPOON);
+        this.floor = new AlchemicIngredient(floorMold, 75, UnitOfQuantity.SPOON);
+        this.containerWall = new IngredientContainer(wall);
+        this.containerFloor = new IngredientContainer(floor);
+        ArrayList<IngredientContainer> containerMix = new ArrayList<>();
+        containerMix.add(containerWall);
+        containerMix.add(containerFloor);
+        this.kettleMix = new Kettle(containerMix);
+
+    }
+
+    @Test
+    public void testConstructorKettle(){
+        ArrayList<AlchemicIngredient> content = new ArrayList<>();
+        content.add(oneIng);
+        Assert.assertEquals(content, kettleOneIng.getContents());
+    }
+
+    @Test
+    public void testUseKettleOneIng(){
+        Assert.assertThrows(IllegalArgumentException.class, () -> kettleOneIng.use());
+    }
+
+    @Test
+    public void testUseKettleSameIng(){
+        kettleFlower.use();
+        AlchemicIngredient newIng = kettleFlower.takeResult();
+        Assert.assertEquals(oneIng.getIngredientType(), newIng.getIngredientType());
+        Assert.assertEquals(moreOfTheSame.getIngredientType(), newIng.getIngredientType());
+        Assert.assertEquals(moreOfTheSame.getSimpleName(), newIng.getSimpleName());
+        Assert.assertFalse(newIng.getIngredientType() instanceof IngredientTypeMixed);
+        Assert.assertEquals(new int[]{50, 0}, newIng.getTemperature());
+        Assert.assertEquals(new int[]{0, 100}, newIng.getIngredientType().getStdTemp());
+        Assert.assertEquals(State.LIQUID, newIng.getState());
+        Assert.assertEquals(State.LIQUID, newIng.getIngredientType().getStdState());
+    }
+
+    @Test
+    public void testUseKettleMix(){
+        kettleMix.use();
+        AlchemicIngredient newIng = kettleFlower.takeResult();
+        Assert.assertEquals("Wall Mold mixed with Floor Mold", newIng.getIngredientType().getName());
+        Assert.assertFalse(wall.getIngredientType() == newIng.getIngredientType());
+        Assert.assertFalse(floor.getIngredientType() == newIng.getIngredientType());
+        Assert.assertTrue(newIng.getIngredientType() instanceof IngredientTypeMixed);
+        Assert.assertEquals(new int[]{5, 0}, newIng.getTemperature());
+        Assert.assertEquals(new int[]{0, 11}, newIng.getIngredientType().getStdTemp());
+        Assert.assertEquals(State.LIQUID, newIng.getState());
+        Assert.assertEquals(State.LIQUID, newIng.getIngredientType().getStdState());
+    }
+
+}
+
+
+
+
